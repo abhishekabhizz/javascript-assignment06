7568c7df1def4bfc8c2b070cf3234674310b8547
abis45 <jinsonnosnij@gmail.com>
2024-11-21 12:02:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+/*function value(input) {
+    return Array.isArray(input);
+    
+}
+console.log(value([1,2,3]));*/
+
+/*function firstelement(arr) {
+    return arr.slice(0,2)
+    
+}
+let myarr=[1,2,3,4,5]
+console.log(firstelement(myarr));*/
+
+/*function lastelement(arr) {
+    return arr.slice(arr.length-1,arr.length)
+    
+}
+let myarr=[1,2,3,4,5,6]
+console.log(lastelement(myarr));*/
+
+
+/*function insertDashes(number) {
+    let numStr = number.toString();
+    let result = numStr.split('').join('-');
+    return result;
+}
+let inputNumber = 925468;
+let output = insertDashes(inputNumber);
+console.log(output);*/
